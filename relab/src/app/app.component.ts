28b47831b81666cf8fae7ae3142a75cc4bf200a1
Fabio Malizia <malizia.fabio@iisgalvanimi.edu.it>
2021-09-21 12:51:47 +0000

    work in progress ... aggiornamento ad google-maps nativa

---
+++ relab/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'server mappe';
+  //Aggiungiamo latitudine e longitudine di un luogo
+  center: google.maps.LatLngLiteral;
+  position: google.maps.LatLngLiteral;
+  label: string;
+  circleOptions: google.maps.CircleOptions;
+  markerOptions: google.maps.MarkerOptions;
+  vertices: google.maps.LatLngLiteral[];
+  constructor() {
+    this.center = { lat: 45.506738, lng: 9.190766 };
+    this.position = this.center;
+    this.label = "ciao";
+    this.circleOptions = { fillColor: 'red' }
+    let iconData: google.maps.Icon = {
+      url: './assets/img/cat_acrobat.ico',
+      scaledSize: new google.maps.Size(60, 60)
+    }
+
+    this.vertices = [
+      {  lat: this.center.lat + 0.001, lng: this.center.lng - 0.002 },
+      {  lat: this.center.lat, lng: this.center.lng },
+      {  lat: this.center.lat - 0.001, lng: this.center.lng - 0.002}
+    ];
+
+    this.markerOptions = { icon: iconData }
+  }
+
+}
