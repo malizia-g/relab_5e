bfd3c112faf9b9ac78685aa40f192132d33acaf6
Fabio Malizia <malizia.fabio@iisgalvanimi.edu.it>
2021-09-24 09:50:40 +0000

    relab aggiunta gejson lombardia manuale

--- relab/src/app/app.component.ts
+++ relab/src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { ElementRef } from '@angular/core';
 import { AfterViewInit } from '@angular/core';
 import { Component, ViewChild } from '@angular/core';
 import { GoogleMap } from '@angular/google-maps'
+import { GEOJSON, GeoFeatureCollection } from './models/geojson.model';
 
 @Component({
   selector: 'app-root',
@@ -11,34 +11,58 @@ import { GoogleMap } from '@angular/google-maps'
 export class AppComponent implements AfterViewInit {
   title = 'server mappe';
   //Aggiungiamo latitudine e longitudine di un luogo
-  center: google.maps.LatLngLiteral;
-  position: google.maps.LatLngLiteral;
-  label: string;
-  circleOptions: google.maps.CircleOptions;
-  markerOptions: google.maps.MarkerOptions;
-  vertices: google.maps.LatLngLiteral[];
+ 
+  geoJsonObject : GeoFeatureCollection;
+  fillColor: string = "#FF0000";  //Colore delle zone catastali
+  center: google.maps.LatLngLiteral = { lat: 45.506738, lng: 9.190766 };
+  markerList : google.maps.MarkerOptions[];
+  zoom : 8;
+
   constructor() {
-    this.center = { lat: 45.506738, lng: 9.190766 };
-    this.position = this.center;
-    this.label = "ciao";
-    this.circleOptions = { fillColor: 'red' }
-    let iconData: google.maps.Icon = {
-      url: './assets/img/cat_acrobat.ico',
-      scaledSize: new google.maps.Size(60, 60)
-    }
 
-    this.vertices = [
-      {  lat: this.center.lat + 0.001, lng: this.center.lng - 0.002 },
-      {  lat: this.center.lat, lng: this.center.lng },
-      {  lat: this.center.lat - 0.001, lng: this.center.lng - 0.002}
-    ];
-
-    this.markerOptions = { icon: iconData }
+    //Questi dati dovremmo scaricarli dal server, per ora li abbiamo copiati nel file gojson.model.ts
+    this.geoJsonObject = GEOJSON;  
+    console.log(this.geoJsonObject); //stampo l'oggetto geoJsonObject sulla console
+    this.markerGenerator();
   }
 
   @ViewChild('mapRef') mapRef: GoogleMap;
   ngAfterViewInit() {
-    console.log(this.mapRef.data.loadGeoJson("https://storage.googleapis.com/mapsdevsite/json/google.json"));
+    this.mapRef.data.addGeoJson(this.geoJsonObject);
+    this.mapRef.data.setStyle(this.styleFunc);
   }
 
+  styleFunc = (feature: any) =>{
+    console.log(feature.i.id)
+    let newColor = "#FF0000"; //RED
+    if(feature.i.id == 0) newColor = "#00FF00"; //GREEN
+    else newColor = "#0000FF"; //BLUE
+    return ({
+      clickable: false,
+      fillColor: newColor,
+      strokeWeight: 1
+    });
+  }
+
+  markerGenerator()
+  {
+    this.markerList =[
+      {
+        position : {
+          lng : this.geoJsonObject.features[0].geometry.coordinates[0][0][0],
+          lat : this.geoJsonObject.features[0].geometry.coordinates[0][0][1]
+        },
+        label: String(this.geoJsonObject.features[0].properties.id),
+      },
+      {
+        position : {
+          lng : this.geoJsonObject.features[1].geometry.coordinates[0][0][0],
+          lat : this.geoJsonObject.features[1].geometry.coordinates[0][0][1]
+        },
+        label: String(this.geoJsonObject.features[1].properties.id),
+      },
+    ]
+
+  }
+ 
 }
