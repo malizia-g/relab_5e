0bbee7eb2ed65264c9cc49a89c0fb6dc05eaf8d8
Fabio Malizia <malizia.fabio@iisgalvanimi.edu.it>
2021-10-02 17:20:09 +0000

    aggiunte icone

--- relab/src/app/app.component.ts
+++ relab/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { HttpClient } from '@angular/common/http';
-import { AfterViewInit } from '@angular/core';
+import { AfterViewInit, Sanitizer } from '@angular/core';
 import { Component, ViewChild } from '@angular/core';
 import { GoogleMap } from '@angular/google-maps'
 import { Observable } from 'rxjs';
 import { Ci_vettore } from './models/ci_vett.model';
-import {  GeoFeatureCollection } from './models/geojson.model';
+import { GeoFeatureCollection } from './models/geojson.model';
 
 @Component({
   selector: 'app-root',
@@ -14,16 +14,16 @@ import {  GeoFeatureCollection } from './models/geojson.model';
 export class AppComponent implements AfterViewInit {
   title = 'server mappe';
   //Variabile che conterrà i nostri oggetti GeoJson
-  geoJsonObject : GeoFeatureCollection;
+  geoJsonObject: GeoFeatureCollection;
   //Observable per richiedere al server python i dati sul DB
   obsGeoData: Observable<GeoFeatureCollection>;
   // Centriamo la mappa
   center: google.maps.LatLngLiteral = { lat: 45.506738, lng: 9.190766 };
-  zoom : 8;
+  zoom: 8;
 
-  markerList : google.maps.MarkerOptions[];
+  markerList: google.maps.MarkerOptions[];
   obsCiVett: Observable<Ci_vettore[]>;
-  
+
   constructor(public http: HttpClient) {
     //Facciamo iniettare il modulo HttpClient dal framework Angular (ricordati di importare la libreria)
   }
@@ -31,26 +31,36 @@ export class AppComponent implements AfterViewInit {
   //Metodo che scarica i dati nella variabile geoJsonObject
   prepareData = (data: GeoFeatureCollection) => {
     this.geoJsonObject = data
-    console.log( this.geoJsonObject );
+    console.log(this.geoJsonObject);
   }
 
-  prepareCiVettData = (data: Ci_vettore[]) =>
-  {
+  prepareCiVettData = (data: Ci_vettore[]) => {
     console.log(data); //Verifica di ricevere i vettori energetici
     this.markerList = []; //NB: markers va dichiarata tra le proprietà markers : Marker[]
     for (const iterator of data) { //Per ogni oggetto del vettore creo un Marker
-      let m : google.maps.MarkerOptions = 
+      let m: google.maps.MarkerOptions =
       {
-       position : new google.maps.LatLng (iterator.WGS84_X, iterator.WGS84_Y),
-       label : iterator.CI_VETTORE 
+        position: new google.maps.LatLng(iterator.WGS84_X, iterator.WGS84_Y),
+        icon : this.findImage(iterator.CI_VETTORE)
       }
       //Marker(iterator.WGS84_X,iterator.WGS84_Y,iterator.CI_VETTORE);
       this.markerList.push(m);
     }
- }
+  }
+
+  findImage(label: string) : google.maps.Icon {
+    if (label.includes("Gas")) {
+      return { url: './assets/img/gas.ico', scaledSize: new google.maps.Size(32,32) };
+    }
+    if (label.includes("elettrica")) {
+      return { url: './assets/img/electricity.ico', scaledSize: new google.maps.Size(32,32) };
+    }
+    //Se non viene riconosciuta nessuna etichetta ritorna l'icona undefined
+      return {url: './assets/img/undefined.ico' , scaledSize: new google.maps.Size(32,32)}
+  }
 
 
- //Una volta che la pagina web è caricata, viene lanciato il metodo ngOnInit scarico i    dati 
+  //Una volta che la pagina web è caricata, viene lanciato il metodo ngOnInit scarico i    dati 
   //dal server
   ngOnInit() {
     this.obsGeoData = this.http.get<GeoFeatureCollection>("https://5000-fuchsia-reindeer-psolbb4g.ws-eu18.gitpod.io/ci_vettore/50");
@@ -67,10 +77,10 @@ export class AppComponent implements AfterViewInit {
     this.mapRef.data.setStyle(this.styleFunc);
   }
 
-  styleFunc = (feature: any) =>{
+  styleFunc = (feature: any) => {
     console.log(feature.i.id)
     let newColor = "#FF0000"; //RED
-    if(feature.i.id == 0) newColor = "#00FF00"; //GREEN
+    if (feature.i.id == 0) newColor = "#00FF00"; //GREEN
     else newColor = "#0000FF"; //BLUE
     return ({
       clickable: false,
@@ -99,5 +109,5 @@ export class AppComponent implements AfterViewInit {
     ]
 
   }*/
- 
+
 }
